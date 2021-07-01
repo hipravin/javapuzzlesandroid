ae128bd2564714c483c63d100455cd5be4398815
Alex <alexander.vmk@gmail.com>
2021-07-01 18:23:23 +0300

    initial empty project

---
+++ app/src/main/java/hipravin/javapuzzles/MainActivity.java
@@ -0,0 +1,13 @@
+package hipravin.javapuzzles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
